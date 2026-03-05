86d2afca9753ade6cf5e33e861b8e077e78dab6f
veronika <sustv153@gmail.com>
2026-03-05 16:23:16 +0200

    add react

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
+import App from "./components/App/App";
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import "modern-normalize/modern-normalize.css";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
